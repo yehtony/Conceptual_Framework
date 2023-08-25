4c4eda0600c5b26ee722239b9447635b9d7ff45c
yehtony <yehtony880208@outlook.com>
2023-08-26 00:08:58 +0800

    DB Sequelize Success

--- db_schemas/app/config/db.config.js
+++ db_schemas/app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: '172.18.0.2', // Host Name
+  HOST: 'localhost', // Host Name
   USER: 'yexuan', // User Name
   PASSWORD: '110524031', // Password
   DB: 'postgres', // Database Name
-  post: 5432,
+  port: 5432,
   DIALECT: 'postgres', // 資料庫類別
   pool: {
     max: 5, // 連結池中最大的 connection 數
